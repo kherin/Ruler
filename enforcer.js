cde79d46fe3863d2998a4f0757d1be72fdc22bed
Kherin <kherin.j.s.bundhoo@gmail.com>
2020-08-05 22:33:51 +0400

    changed exit strategy

--- enforcer.js
+++ enforcer.js
@@ -6,7 +6,7 @@ const choices = ['Option 1', 'Option 2', 'Option 3'];
 
 (async function () {
 
-    inquirer
+    const answers = await inquirer
         .prompt([
             {
                 type: 'checkbox',
@@ -14,19 +14,9 @@ const choices = ['Option 1', 'Option 2', 'Option 3'];
                 message: 'Coding Standards',
                 choices: choices
             }
-        ])
-        .then(answers => {
-            const exitCode = answers.length == choices.length ? 1 : 0;
-            process.exit(exitCode);
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-                console.log('Could not be rendered');
-            } else {
-                // Something else when wrong
-                console.log('error: ', error);
-            }
-        });
+        ]);
+
+    console.log('answers: ', answers);
+    process.exit(0);
 })();
 
