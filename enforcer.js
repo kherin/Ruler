597f518cb2b5a0867b1b66780e6670712152384c
Kherin <kherin.j.s.bundhoo@gmail.com>
2020-08-05 22:30:53 +0400

    added iif

--- enforcer.js
+++ enforcer.js
@@ -4,25 +4,29 @@ const inquirer = require('inquirer');
 const name = 'standards';
 const choices = ['Option 1', 'Option 2', 'Option 3'];
 
-inquirer
-    .prompt([
-        {
-            type: 'checkbox',
-            name: name,
-            message: 'Coding Standards',
-            choices: choices
-        }
-    ])
-    .then(answers => {
-        const exitCode = answers.length == choices.length ? 1 : 0;
-        process.exit(exitCode);
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-            console.log('Could not be rendered');
-        } else {
-            // Something else when wrong
-            console.log('error: ', error);
-        }
-    });
+(async function () {
+
+    inquirer
+        .prompt([
+            {
+                type: 'checkbox',
+                name: name,
+                message: 'Coding Standards',
+                choices: choices
+            }
+        ])
+        .then(answers => {
+            const exitCode = answers.length == choices.length ? 1 : 0;
+            process.exit(exitCode);
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+                console.log('Could not be rendered');
+            } else {
+                // Something else when wrong
+                console.log('error: ', error);
+            }
+        });
+})();
+
